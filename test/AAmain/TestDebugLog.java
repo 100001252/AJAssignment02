2d273afaf243b68613b6418f602b473777f2ed5c
mansour.sh <100001252@student.swin.edu.au>
2017-10-07 19:48:28 +1100

    changes to start

--- test/AAmain/TestDebugLog.java
+++ test/AAmain/TestDebugLog.java
@@ -5,7 +5,7 @@
  */
 package AAmain;
 
-import helper02.DebugLog;
+import helper.DebugLog;
 import java.util.ArrayList;
 
 /**
@@ -28,8 +28,12 @@ public class TestDebugLog {
             ArrayList<String> lstfile = new ArrayList<String>();
             lstfile.add(s);
             lstfile.add("another one");
+            DebugLog.saveData(lstfile);
             //IO_Support.saveData("C:\\zzzzjava\\aadworks\\javaDebug.txt", lstfile);
-            DebugLog.appendData(lstfile);
+            DebugLog.appendData("here is my first test for debuging 02");
+            DebugLog.appendData("here is my first test for debuging 03");
+            DebugLog.appendData("here is my first test for debuging 04");
+            DebugLog.appendData("here is my first test for debuging 05");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
